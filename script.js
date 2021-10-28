1fedc13930409e8d80e221cf7b4b9773ee13b2e8
IDSAD <idsad.ch@gmail.com>
2021-10-28 10:48:10 +0300

    added: basic menu

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
 
-
+setMenu()
+function setMenu() {
+	const contentBlocks = document.querySelectorAll('.content__block');
+	const menuButtons = document.querySelectorAll('.menu__button')
+	menuButtons.forEach(button => {
+		button.addEventListener("click", (event) => {
+			menuButtons.forEach(btn => {
+				btn === button
+					? btn.classList.add('active')
+					: btn.classList.remove('active')
+			})
+			contentBlocks.forEach(block => {
+				block.id === button.dataset.block
+					? block.classList.add('active')
+					: block.classList.remove('active')
+			})
+		})
+	})
+}
