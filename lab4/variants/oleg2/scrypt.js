dab91d1f4e5168750451e0af567da3914985af0f
IDSAD <idsad.ch@gmail.com>
2021-10-28 15:18:28 +0300

    added: content

---
+++ lab4/variants/oleg2/scrypt.js
@@ -0,0 +1,9 @@
+let array = (Головна, Компанія, Блог, Контакти);
+var valueAtIndex1 = array.getValue(1);
+var valueAtIndex2 = array.getValue(2);
+var valueAtIndex3 = array.getValue(3);
+var valueAtIndex4 = array.getValue(4);
+document.querySelector('#out1').innerHTML = valueAtIndex1;
+document.querySelector('#out2').innerHTML = valueAtIndex2;
+document.querySelector('#out1').innerHTML = valueAtIndex3;
+document.querySelector('#out2').innerHTML = valueAtIndex4;
